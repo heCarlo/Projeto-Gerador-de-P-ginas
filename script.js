476f90c9cb49f44f95725f6840f665e7f5fc8607
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-02-24 19:31:23 -0300

    d

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ function inserir(){
 
     // inserir cdg em js
     let codeJs = document.getElementById("codeJs")
-    document.getElementById("jsUser").value.innerHTML = codeJs
+    (document.getElementById("jsUser").value).innerHTML = codeJs
 
 }
